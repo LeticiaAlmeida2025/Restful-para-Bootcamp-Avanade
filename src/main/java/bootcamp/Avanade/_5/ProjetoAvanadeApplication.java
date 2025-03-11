27f62e254f7ed30fecf2002209225dca0b30a07a
Leticia Faria de Almeida <leticia.faria@aluno.faculdadeimpacta.com.br>
2025-03-10 21:01:43 -0300

    Initial commit

---
+++ src/main/java/bootcamp/Avanade/_5/ProjetoAvanadeApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.Avanade._5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAvanadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAvanadeApplication.class, args);
+	}
+
+}
